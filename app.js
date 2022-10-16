93b25a7fef4dd88f850f013f1f18136d36b3ed09
Arkadiusz N <arkadiusz.nocek@gmail.com>
2022-10-16 21:07:19 +0200

    zad2

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const _ = require("lodash");
 let mean_val = _.mean([15, 7, 38, 46, 82]);
 
 console.log(mean_val);
+
+
+
+let max_val = _.max([15, 7, 38, 46, 82]);
+let min_val = _.min([15, 7, 38, 46, 82])
+console.log(max_val);
+console.log(min_val);
